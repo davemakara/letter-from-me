09ed3b11c2bd459af5c0d5c1795855fd9314448a
davidmakarovv <davidmakarr@gmail.com>
2024-07-26 15:35:17 +0400

    fixing errormessage logic

--- src/components/FormPage.jsx
+++ src/components/FormPage.jsx
@@ -3,13 +3,16 @@ import { useNavigate } from "react-router-dom";
 
 const FormPage = () => {
   const [firstName, setFirstName] = useState("");
+  const [errorMessage, setErrorMessage] = useState(null);
   const navigate = useNavigate();
 
   const formHandler = (e) => {
     e.preventDefault();
 
-    if (firstName.toLowerCase() === "mariam") {
+    if (firstName.toLowerCase() === "მარწყვუნა") {
       navigate("/page1");
+    } else {
+      setErrorMessage(firstName);
     }
   };
 
@@ -24,16 +27,23 @@ const FormPage = () => {
         className="w-[300px] h-[400px] p-5 bg-[#1F2544] border-2 border-white rounded-lg flex flex-col items-center text-white shadow-lg shadow-black"
       >
         <h1 className="text-lg font-semibold mb-16 mt-10 text-center">
-          წერილის სანახავად ჩაწერე შენი სახელი (ინგლისურად).
+          წერილის სანახავად ჩაწერე შენი სახელი.
         </h1>
         <input
           type="text"
           id="first-name"
           name="first-name"
           placeholder="First Name"
-          className="w-full text-lg p-3 mb-5 text-black"
+          className="w-full text-lg p-3 mb-4 text-black"
           onChange={(e) => handleNameChange(e)}
         />
+        <p
+          className={`text-lg font-semibold mb-5 ${
+            errorMessage ? "opacity-100" : "opacity-0"
+          }`}
+        >
+          არასწორი სახელია🙃
+        </p>
         <button
           type="submit"
           className="w-40 h-16 bg-[#9A3B3B] text-white font-semibold text-lg rounded-lg"
